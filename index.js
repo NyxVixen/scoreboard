0f821e38e9c4c213ea356e1c19b046a809a06802
Anastasia Anderson <124330987+NyxVixen@users.noreply.github.com>
2023-06-04 10:42:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let homeScoreBtnOne = document.getElementById("home-score-btn1")
-let homeScoreBtnTwo = document.getElementById("home-score-btn2")
-let homeScoreBtnThree = document.getElementById("home-score-btn3")
-let guestScoreBtnOne = document.getElementById("guest-score-btn1")
-let guestScoreBtnTwo = document.getElementById("guest-score-btn2")
-let guestScoreBtnThree = document.getElementById("guest-score-btn3")
 let resetScore = document.getElementById("new-game-el")
 let homeStoreEl = document.getElementById("home-el")
 let guestStoreEl = document.getElementById("guest-el")
@@ -46,4 +40,3 @@ function newGame() {
     guestStoreEl.textContent = "0"
     guestScore = 0
 }
-
